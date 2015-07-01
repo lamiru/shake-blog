317d329e3189ef711c6583a7b077016911bf058e
lamiru <lamiru@gmail.com>
2015-07-01 15:34:38 +0900

    post_new with form.html template

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'blog.views',
     url(r'^$', 'index', name='index'),
+    url(r'^new/$', 'new', name='post_new'),
 )
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'blog/index.html', {
         })
+
+
+def new(request):
+    return render(request, 'blog/form.html', {
+        })
