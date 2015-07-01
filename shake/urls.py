84aeebec08da5499ab44711baf56aeb73e3e9e62
lamiru <lamiru@gmail.com>
2015-07-01 12:51:56 +0900

    startapp blog & blog:index

--- shake/urls.py
+++ shake/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'shake.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', RedirectView.as_view(pattern_name='blog:index'), name='root'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('blog.urls', namespace='blog')),
 ]
