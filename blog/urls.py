8faeee54662987b129e7a97864f290a2f8ea07b3
lamiru <lamiru@gmail.com>
2015-07-02 17:17:14 +0900

    post_detail

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns(
     'blog.views',
     url(r'^$', 'index', name='index'),
     url(r'^new/$', 'new', name='post_new'),
-    url(r'^(?P<id>\d+)/edit/$', 'edit', name='post_edit')
+    url(r'^(?P<id>\d+)/edit/$', 'edit', name='post_edit'),
+    url(r'^(?P<id>\d+)/$', 'detail', name='post_detail'),
 )
--- blog/views.py
+++ blog/views.py
@@ -14,8 +14,8 @@ def new(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('blog:index')
+            post = form.save()
+            return redirect('blog:post_detail', post.id)
     else:
         form = PostForm()
     return render(request, 'blog/form.html', {
@@ -30,9 +30,16 @@ def edit(request, id):
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             form.save()
-            return redirect('blog:index')
+            return redirect('blog:post_detail', post.id)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/form.html', {
         'form': form,
+    })
+
+
+def detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'blog/post_detail.html', {
+        'post': post,
     })
