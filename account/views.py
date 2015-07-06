652df4476eff622994246378312eeb4af653c094
lamiru <lamiru@gmail.com>
2015-07-06 17:50:48 +0900

    auto login after signup

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth import authenticate, login as auth_login
 from django.views.generic import CreateView
 
 
@@ -10,5 +11,10 @@ class SignupView(CreateView):
     success_url = reverse_lazy('blog:index')
 
     def form_valid(self, form):
+        response = super(SignupView, self).form_valid(form)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password1']
+        user = authenticate(username=username, password=password)
+        auth_login(self.request, user)
         messages.info(self.request, 'You are now registered.')
-        return super(SignupView, self).form_valid(form)
+        return response
