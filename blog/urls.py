24cfb3c08c79cf46e91576d54985cbcb27244e6b
lamiru <lamiru@gmail.com>
2015-07-12 08:46:10 +0900

    blog:post_detail cbv

--- blog/urls.py
+++ blog/urls.py
@@ -4,13 +4,13 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'blog.views_cbv',
     url(r'^$', 'index', name='index'),
+    url(r'^(?P<id>\d+)/$', 'detail', name='post_detail'),
 )
 
 urlpatterns += patterns(
     'blog.views',
     url(r'^new/$', 'new', name='post_new'),
     url(r'^(?P<id>\d+)/edit/$', 'edit', name='post_edit'),
-    url(r'^(?P<id>\d+)/$', 'detail', name='post_detail'),
     url(r'^(?P<id>\d+)/delete/$', 'delete', name='post_delete'),
     url(r'^(?P<id>\d+)/comments/new/$', 'comment_new', name='comment_new'),
     url(r'^(?P<id>\d+)/comments/(?P<comment_id>\d+)/edit/$', 'comment_edit',
--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -1,5 +1,19 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from blog.models import Post
+from blog.forms import CommentForm
 
 
 index = ListView.as_view(model=Post, template_name='blog/index.html')
+
+
+class PostDetailView(DetailView):
+    model = Post
+    pk_url_kwarg = 'id'
+    template_name = 'blog/post_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()
+        return context
+
+detail = PostDetailView.as_view()
